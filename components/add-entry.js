9a92461a1e2e6d62066ab71622a2eba645769c8e
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-24 17:47:22 -0500

    add-entry component

--- components/add-entry.js
+++ components/add-entry.js
@@ -3,6 +3,38 @@ import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  };
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+    this.setState((state) => {
+      const count = state[metric] + step;
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      }
+    });
+  }
+  decrement = (metric) => {
+    const { step } = getMetricMetaInfo(metric);
+    this.setState((state) => {
+      const count = state[metric] - step;
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count,
+      }
+    });
+  }
+  slide = (metric, slide) => {
+    this.setState(() => ({
+      [metric]: value,
+    }))
+  }
   render() {
     return (
       <View>
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { white } from './colors';
+import { red, orange, blue, lightPurp, pink, white } from './colors'
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20
+  },
+})
 
 export function getMetricMetaInfo(metric) {
   const info = {
